c97fae8bc82c5b9a2a6c6c7724b74029a22c6fb6
Jack Irish <irish.l.jack@gmail.com>
2018-01-20 13:03:26 -0500

    Added Player Prefab, with movement.

---
+++ JetSwapTest/Assets/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [Header("Horizontal Movement")]
+    [SerializeField] string xHorizontalAxisName;
+    [SerializeField] string zHorizontalAxisName;
+    [Range(1, 20)] [SerializeField] float movementSpeed;
+
+    [Header("Joystick Number")]
+    [SerializeField]
+    int joystick;
+
+    private void Movement() {
+        float xThrow = Input.GetAxis(xHorizontalAxisName);
+        float xMovement = xThrow * movementSpeed * Time.deltaTime;
+        float xPos = transform.position.x + xMovement;
+
+        float zThrow = Input.GetAxis(zHorizontalAxisName);
+        float zMovement = zThrow * movementSpeed * Time.deltaTime;
+        float zPos = transform.position.z + zMovement * -1;
+
+       
+        transform.position = new Vector3(xPos,
+                                        transform.position.y,
+                                         zPos);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+}
