7277ac22026be34a2fe4df88e0b41f452667dad3
doome1337 <dmitry.paramonov@gmail.com>
2018-01-20 12:09:14 -0500

    Beginning the work on swapping.

---
+++ JetSwapTest/Assets/swap_effects/swappable.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class swappable : MonoBehaviour {
+    public void UpdateSwap (swapper other, float swap_completion) {
+    }
+
+    public float GetSwapResistance () {
+        return 1.0;
+    }
+
+    public void DisengageSwap () {
+    }
+}
