a898b51a80c48da51036a3b99c9abcf1b92a8edd
Jack Irish <irish.l.jack@gmail.com>
2018-01-20 14:21:22 -0500

    Added Player movement.

--- JetSwapTest/Assets/Player/PlayerMovement.cs
+++ JetSwapTest/Assets/Player/PlayerMovement.cs
@@ -2,40 +2,61 @@
 
 public class PlayerMovement : MonoBehaviour {
 
+    [Header("Game Objects Needed")]
+
     [Header("Horizontal Movement")]
     [SerializeField] string xHorizontalAxisName;
     [SerializeField] string zHorizontalAxisName;
     [Range(1, 20)] [SerializeField] float movementSpeed;
+
+    [Header("Rotation Movement")]
+    [Range(150, 500)][SerializeField] float rotationSpeed;
+    [SerializeField] string xHorizontalRotationAxisName;
+    [SerializeField] string yVerticalRotationAxisName;
 
     [Header("Joystick Number")]
     [SerializeField]
     int joystick;
     public Ray r;
 
-    private void Movement() {
+    private void UserMovement() {
         float xThrow = Input.GetAxis(xHorizontalAxisName);
         float xMovement = xThrow * movementSpeed * Time.deltaTime;
-        float xPos = transform.position.x + xMovement;
 
         float zThrow = Input.GetAxis(zHorizontalAxisName);
         float zMovement = zThrow * movementSpeed * Time.deltaTime;
-        float zPos = transform.position.z + zMovement * -1;
 
-       
-        transform.position = new Vector3(xPos,
-                                        transform.position.y,
-                                         zPos);
         r = new Ray(transform.position, transform.forward);
         GetComponent<swapper>().shoot(r);
+
+        transform.position += transform.rotation * new Vector3(xMovement, 0, -zMovement);
+        transform.position = new Vector3(transform.position.x,
+                                         0,
+                                         transform.position.z);
+    }
+
+    private void RotationMovement() {
+        float xThrow = Input.GetAxis(xHorizontalRotationAxisName);
+        float xMovement = xThrow * rotationSpeed * Time.deltaTime;
+        float xPos = transform.eulerAngles.y + xMovement;
+
+        float yThrow = Input.GetAxis(yVerticalRotationAxisName);
+        float yMovement = yThrow * rotationSpeed * Time.deltaTime;
+        float yPos = transform.eulerAngles.x + yMovement;
+
+        transform.eulerAngles = new Vector3(yPos, xPos, transform.eulerAngles.z);
     }
 
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Movement();
+        UserMovement();
+        RotationMovement();
 	}
+
+
 }
