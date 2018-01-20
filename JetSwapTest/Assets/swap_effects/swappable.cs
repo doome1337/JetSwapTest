6cb1154a2e93778007984f512091823fbdfcf701
doome1337 <dmitry.paramonov@gmail.com>
2018-01-20 12:19:17 -0500

    Basic swappable.

--- JetSwapTest/Assets/swap_effects/swappable.cs
+++ JetSwapTest/Assets/swap_effects/swappable.cs
@@ -3,13 +3,34 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class swappable : MonoBehaviour {
+    public float swap_resistance = 0.0f;
+    private float max_swap_completion = 0.0f;
+    private int swapless_time = 0;
+
     public void UpdateSwap (swapper other, float swap_completion) {
+        if (swap_completion >= max_swap_completion) {
+            max_swap_completion = swap_completion;
+            UpdateSwapInternal(other, swap_completion);
+        }
+        swapless_time = 0;
+    }
+
+    private void UpdateSwapInternal (swapper other, float swap_completion) {
     }
 
     public float GetSwapResistance () {
-        return 1.0f;
+        return swap_resistance;
     }
 
     public void DisengageSwap () {
+        max_swap_completion = 0.0f;
+        swapless_time = 0;
+    }
+
+    void Update () {
+        swapless_time++;
+        if (swapless_time > 60) {
+            DisengageSwap();
+        }
     }
 }
