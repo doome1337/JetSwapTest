ee2f386a2ffdea73b38168f1cd39c3b932de1114
Jack Irish <irish.l.jack@gmail.com>
2018-01-21 21:56:27 -0500

    Added player and fixed player movement, plus rotation.

--- JetSwapTest/Assets/Player/PlayerMovement.cs
+++ JetSwapTest/Assets/Player/PlayerMovement.cs
@@ -1,8 +1,11 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
     [Header("Game Objects Needed")]
+    [SerializeField] GameObject head;
+    [SerializeField] GameObject player;
 
     [Header("Horizontal Movement")]
     [Range(1, 20)] [SerializeField] float movementSpeed;
@@ -20,6 +23,17 @@ public class PlayerMovement : MonoBehaviour {
     private string RSLeftRight = "RSLeftRight";
     private string RSUpDown = "RSUpDown";
 
+    private float furthestHeadUp = -95;
+    private float furthestHeadDown = -35;
+
+    private enum HeadTopBottom {
+        Top,
+        Bottom,
+        Neither
+    }
+
+    private HeadTopBottom headLoc = HeadTopBottom.Neither;
+
     private void UserMovement() {
         float xThrow = Input.GetAxis(LSLeftRight + joystick);
         float xMovement = xThrow * movementSpeed * Time.deltaTime;
@@ -27,32 +41,76 @@ public class PlayerMovement : MonoBehaviour {
         float zThrow = Input.GetAxis(LSUpDown + joystick);
         float zMovement = zThrow * movementSpeed * Time.deltaTime;
 
-        r = new Ray(transform.position, transform.forward);
-        GetComponent<swapper>().shoot(r);
+        //r = new Ray(transform.position, transform.forward);
+        //GetComponent<swapper>().shoot(r);
 
         transform.position += transform.rotation * new Vector3(xMovement, 0, -zMovement);
 
         //Doesn't allow player to go upward's, if they're looking up.
-        transform.position = new Vector3(transform.position.x,
-                                         0,
-                                         transform.position.z);
+        //transform.position = new Vector3(transform.position.x,
+                                         //0,
+                                         //transform.position.z);
     }
 
     private void RotationMovement() {
-        float xThrow = Input.GetAxis(RSLeftRight + joystick);
-        float xMovement = xThrow * rotationSpeed * Time.deltaTime;
-        float xPos = transform.eulerAngles.y + xMovement;
-
-        float yThrow = Input.GetAxis(RSUpDown + joystick);
-        float yMovement = yThrow * rotationSpeed * Time.deltaTime;
-        float yPos = transform.eulerAngles.x + yMovement;
-
-        transform.eulerAngles = new Vector3(yPos, xPos, transform.eulerAngles.z);
+        RotateLeftRight();
+        LookUpDown();
     }
 
-	// Use this for initialization
-	void Start () {
-        
+    private void RotateLeftRight() {
+        float xThrow = 0;
+        if (Input.GetAxis(RSLeftRight + joystick) < 0) {
+            xThrow = -rotationSpeed;
+        }
+        else if (Input.GetAxis(RSLeftRight + joystick) > 0) {
+            xThrow = rotationSpeed;
+        }
+        player.transform.Rotate(Vector3.down * Time.deltaTime * xThrow * -1);
+    }
+
+    private void LookUpDown() {
+        float yThrow = 0;
+        if (Input.GetAxis(RSUpDown + joystick) < 0) {
+            yThrow = -rotationSpeed;
+
+        }
+        else if (Input.GetAxis(RSUpDown + joystick) > 0) {
+            yThrow = rotationSpeed;
+        }
+        Vector3 rotateHead = Vector3.left * Time.deltaTime * yThrow * -1;
+        Quaternion currLocRotation = head.transform.localRotation;
+        GameObject tempHead = head;
+
+        //if ((int)(currLocRotation.x * 100) < furthestHeadUp) {
+        //    player.transform.Rotate(Vector3.left * Time.deltaTime * yThrow * -1);
+        //    print("Here");
+        //}
+        //else if ((int)(currLocRotation.z * 100) > furthestHeadRight && currMovement == LeftRightMovement.Right)
+        //{
+        //    player.transform.Rotate(Vector3.down * Time.deltaTime * yThrow * -1);
+        //}
+        //else
+        //{
+        if((int)(currLocRotation.x * 100) < furthestHeadUp) {
+            headLoc = HeadTopBottom.Top;
+        }
+        else if((int)(currLocRotation.x * 100) > furthestHeadDown) {
+            headLoc = HeadTopBottom.Bottom;
+        }
+
+        if((headLoc == HeadTopBottom.Top && yThrow > 0) ||
+                (headLoc == HeadTopBottom.Bottom && yThrow < 0) ||
+                headLoc == HeadTopBottom.Neither){
+
+            tempHead.transform.Rotate(rotateHead);
+            headLoc = HeadTopBottom.Neither;
+        }
+            
+        //}
+    }
+
+    // Use this for initialization
+    void Start () {
 	}
 	
 	// Update is called once per frame
